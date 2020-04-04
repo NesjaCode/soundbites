669905913346bd5f1e644d14d9f9b81009d6355a
NesjaCode <willemen.jannes@hotmail.com>
2020-04-04 15:49:10 +0200

    test opening facebook (browser/app)

---
+++ assets/js/social.js
@@ -0,0 +1,4 @@
+function launchFacebook() {
+    setTimeout(function () { window.location = "https://www.facebook.com/Miguel.de.Oliveira.e.silva"; }, 25);
+    window.location = "fb://Miguel.de.Oliveira.e.silva";
+}
