e94cdf76e6bfd0093bf4ea1c0a40931d37665426
NesjaCode <willemen.jannes@hotmail.com>
2020-04-04 16:30:47 +0200

    jQuery test

--- assets/js/social.js
+++
@@ -1,4 +0,0 @@
-function launchFacebook() {
-    setTimeout(function () { window.location = "https://www.facebook.com/Miguel.de.Oliveira.e.silva"; }, 25);
-    window.location = "fb://Miguel.de.Oliveira.e.silva";
-}
